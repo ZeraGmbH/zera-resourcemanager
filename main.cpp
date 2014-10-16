726e5db5f9880d5e58da5d47e5409d07bc5542dc
Samuel Stirtzel <s.stirtzel@googlemail.com>
2014-10-16 10:15:59 +0200

    Switch to QCoreApplication to enable early startup (before X server)
    Signed-off-by: Samuel Stirtzel <s.stirtzel@googlemail.com>

--- main.cpp
+++ main.cpp
@@ -5,22 +5,14 @@
 
 int main(int argc, char* argv[])
 {
-  QApplication a(argc,argv);
+  QCoreApplication a(argc,argv);
 
   //QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8")); // Protobuf does not like latin-1
 
-  ResourceManager* rManager=0;
-  SCPI::SCPIInterface* scpiIface=0;
-  Server::ServerInterface* sInterface=0;
 
-  rManager=ResourceManager::getInstance();
-  scpiIface=SCPI::SCPIInterface::getInstance();
-  sInterface=Server::ServerInterface::getInstance();
-
-  // we only need to call the constructor of these, the rest is handled by the qt even queue, but gcc thinks the variables are unused objects
-  Q_UNUSED(rManager)
-  Q_UNUSED(scpiIface)
-  Q_UNUSED(sInterface)
+  ResourceManager::getInstance();
+  SCPI::SCPIInterface::getInstance();
+  Server::ServerInterface::getInstance();
 
   return a.exec();
 }
