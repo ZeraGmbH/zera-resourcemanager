a0013c4eab9e1022ccd49b83b92022d041fa4a0b
Andreas Müller <schnitzeltony@gmail.com>
2020-12-16 09:02:16 +0100

    rmprotobufwrapper: Fix deprecation warning
    Fixes:
    <...>/rmprotobufwrapper.cpp:24: warning: 'ByteSize' is deprecated: Please use ByteSizeLong() instead
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- rmprotobufwrapper.cpp
+++ rmprotobufwrapper.cpp
@@ -21,5 +21,5 @@ std::shared_ptr<google::protobuf::Message> RMProtobufWrapper::byteArrayToProtobu
 
 QByteArray RMProtobufWrapper::protobufToByteArray(const google::protobuf::Message &t_protobufMessage)
 {
-  return QByteArray(t_protobufMessage.SerializeAsString().c_str(), t_protobufMessage.ByteSize());
+  return QByteArray(t_protobufMessage.SerializeAsString().c_str(), t_protobufMessage.ByteSizeLong());
 }
