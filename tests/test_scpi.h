369f956b4122b2a5d96413cde233c7cc8cdbd12a
Andreas Müller <schnitzeltony@gmail.com>
2022-11-27 22:39:25 +0100

    Work more on scpi tests with interesting results...
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/test_scpi.h
+++ tests/test_scpi.h
@@ -1,20 +1,43 @@
 #ifndef TEST_SCPI_H
 #define TEST_SCPI_H
 
+#include "resourcemanager.h"
+#include "resman_scpiinterface.h"
+#include "clientmultitontest.h"
 #include <QObject>
 
 class test_scpi : public QObject
 {
     Q_OBJECT
 private slots:
+    void init();
+    void cleanup();
+
     void addValidResource();
     void addValidResourceAndCatalog();
     void addValidResourceAndCatalogType();
     void addValidResourceTwice();
+    void addValidResourceAndRemove();
+    void addValidResourceAndQuery();
+    void addNestedAndQuery(); // TODO: sub nodes can be entered but will not be found anymore
+    void addNestedAsParamAndQuery(); // TODO: sub nodes can be entered but will not be found anymore
+    void addValidResourceAndSetAndQuery();
+    void addValidResourceAndSetAndFreeAndQuery(); // TODO: free ignores additional parameters
+
     void addInvalidParamCount();
+    void addValidAndRemoveInvalidSub();
     void addInvalidPort();
     void invalidScpi();
+    void freeOnEmpty();
+    void setMoreThanAvailable();
+    void freeUnset();
+    void freeMoreThanSet(); // TODO: free ignores additional parameters
 
+private:
+    void sendScpi(const char *cmd);
+    ResourceManager *m_resMan;
+    SCPI::SCPIInterface *m_scpiInterface;
+    ClientMultitonTest *m_client;
 };
 
 #endif // TEST_SCPI_H
