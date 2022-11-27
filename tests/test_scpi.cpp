c60dbffdf2c46a9ef810566d9a6ca10b1945f4bb
Andreas Müller <schnitzeltony@gmail.com>
2022-11-27 21:08:03 +0100

    test_scpi: Add most simple error tests
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- tests/test_scpi.cpp
+++ tests/test_scpi.cpp
@@ -19,3 +19,45 @@ void test_scpi::addValidResource()
     scpiInterface.onScpiTransaction(&client, scpiCmd);
     QCOMPARE(client.getAckList().count(), 1);
 }
+
+void test_scpi::addInvalidParamCount()
+{
+    ResourceManager resMan;
+    SCPI::SCPIInterface scpiInterface(&resMan);
+    Application::ResourceIdentity::setSCPIInterface(&scpiInterface);
+    ClientMultitonTest client("fooName", "fooIp");
+
+    ProtobufMessage::NetMessage_ScpiCommand scpiCmd;
+    scpiCmd.set_command("RESOURCE:ADD SENSE;FOONAME;1;FOODESC;");
+
+    scpiInterface.onScpiTransaction(&client, scpiCmd);
+    QCOMPARE(client.getNackList().count(), 1);
+}
+
+void test_scpi::addInvalidPort()
+{
+    ResourceManager resMan;
+    SCPI::SCPIInterface scpiInterface(&resMan);
+    Application::ResourceIdentity::setSCPIInterface(&scpiInterface);
+    ClientMultitonTest client("fooName", "fooIp");
+
+    ProtobufMessage::NetMessage_ScpiCommand scpiCmd;
+    scpiCmd.set_command("RESOURCE:ADD SENSE;FOONAME;1;FOODESC;PORT;");
+
+    scpiInterface.onScpiTransaction(&client, scpiCmd);
+    QCOMPARE(client.getNackList().count(), 1);
+}
+
+void test_scpi::invalidScpi()
+{
+    ResourceManager resMan;
+    SCPI::SCPIInterface scpiInterface(&resMan);
+    Application::ResourceIdentity::setSCPIInterface(&scpiInterface);
+    ClientMultitonTest client("fooName", "fooIp");
+
+    ProtobufMessage::NetMessage_ScpiCommand scpiCmd;
+    scpiCmd.set_command("RESO:ADD SENSE;FOONAME;1;FOODESC;PORT;");
+
+    scpiInterface.onScpiTransaction(&client, scpiCmd);
+    QCOMPARE(client.getErrList().count(), 1);
+}
