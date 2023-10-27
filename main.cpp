e1d57e9f77e9b3bb6a598a4bdb70be14f1267ce2
Andreas Müller <schnitzeltony@gmail.com>
2023-10-27 22:16:30 +0200

    main.cpp: Cleanup
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 int main(int argc, char* argv[])
 {
     QCoreApplication a(argc,argv);
-
-    //QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8")); // Protobuf does not like latin-1
-
     ResmanRunFacade runner;
     return a.exec();
 }
