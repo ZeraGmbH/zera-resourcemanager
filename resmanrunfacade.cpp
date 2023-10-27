1da12c377b606a974955497491ca063378ea87bc
Andreas Müller <schnitzeltony@gmail.com>
2023-10-27 22:00:39 +0200

    Remove resman-lib - it moved to zenux-legacy-components
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- resmanrunfacade.cpp
+++
@@ -1,9 +0,0 @@
-#include "resmanrunfacade.h"
-
-ResmanRunFacade::ResmanRunFacade() :
-    m_scpiInterface(&m_resMan),
-    m_serverInterface(&m_scpiInterface)
-{
-    Application::ResourceIdentity::setSCPIInterface(&m_scpiInterface);
-    m_serverInterface.start();
-}
