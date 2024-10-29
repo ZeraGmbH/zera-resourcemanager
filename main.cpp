98e26c77b56a9d392f5ca507830b33d8e14c79ea
Andreas Müller <schnitzeltony@gmail.com>
2024-10-29 10:00:01 +0100

    Follow changes in zenux-legacy-components and inject tcp network factory
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "resmanrunfacade.h"
+#include <tcpnetworkfactory.h>
 #include <QCoreApplication>
 
 int main(int argc, char* argv[])
 {
     QCoreApplication a(argc,argv);
-    ResmanRunFacade runner;
+    ResmanRunFacade runner(VeinTcp::TcpNetworkFactory::create());
     return a.exec();
 }
